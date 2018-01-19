14b988eb2920f716b537e01cda9358bac88410eb
cathyatseneca <cathyatseneca@gmail.com>
2018-01-19 13:14:18 -0500

    factorial, power and fib base

---
+++ factpowfib.cpp
@@ -0,0 +1,22 @@
+//this function returns n!
+//n! = n * (n-1) * (n-2) * ... * 1
+//1! = 1
+//0! = 1
+unsigned int factorial(unsigned int n){
+	
+}
+//returns base^n = base*base*base...*base 
+//(base multiplied by itself n times)
+double power(double base, unsigned int n){
+	
+}
+//returns the nth fibonacci number
+//0th fibonacci number is 0
+//1st fibonacci number is 1
+//2nd fibonacci number is 0 + 1 = 1
+//3rd fibonacci number is 1 + 1 = 2
+//nth fibonacci number is the sum of the two
+//previous (n-1)th and (n-2)th fibonacci numbers
+unsigned int fibonacci(unsigned int n){
+	
+}
